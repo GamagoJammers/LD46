eaef80128f9a2e8b90ca29530b33cc6f6a5bc994
LucasPejoux <lucas.pejoux.03@hotmail.fr>
2020-04-18 13:41:50 +0200

    ui

--- Assets/UITimer.cs
+++ Assets/UITimer.cs
@@ -6,17 +6,21 @@ using UnityEngine;
 public class UITimer : MonoBehaviour
 {
     private float Timer = 0.0f;
+    float minutes;
+    float seconds;
     public Text timerText;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Timer += Time.deltaTime;
-        timerText.text = Timer.ToString();
+        seconds = Timer%60;
+        minutes = Timer / 60;
+        timerText.text = minutes.ToString("00") + ":" + seconds.ToString("00");
     }
 }
