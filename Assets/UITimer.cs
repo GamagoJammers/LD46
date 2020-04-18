7be0dd51f1e4c52725d9d278473a47d7e682fce2
LucasPejoux <lucas.pejoux.03@hotmail.fr>
2020-04-18 13:14:24 +0200

    ui

---
+++ Assets/UITimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UITimer : MonoBehaviour
+{
+    private float Timer = 0.0f;
+    public Text timerText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        timerText.text = Timer.ToString();
+    }
+}
