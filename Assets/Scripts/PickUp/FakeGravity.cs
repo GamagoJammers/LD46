5f92a85f5129b9e5b2fef8936e96397ba16b1693
Eva <eva.gerbert.gaillard@gmail.com>
2020-04-19 08:58:47 +0200

    Tweak item (throw + drop + gravity) + fix picker

---
+++ Assets/Scripts/PickUp/FakeGravity.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FakeGravity : MonoBehaviour
+{
+    public float m_fakeGravity;
+    public Rigidbody m_rb;
+
+    void Start()
+    {
+        m_rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!m_rb.isKinematic )
+        {
+            m_rb.AddForce(m_fakeGravity * Vector3.down, ForceMode.Acceleration);
+        }
+    }
+}
