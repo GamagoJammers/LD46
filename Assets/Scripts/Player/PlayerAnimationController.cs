75a92201948d0a4a72022ed46fec01b0c2a303e9
Eva <eva.gerbert.gaillard@gmail.com>
2020-04-19 20:32:55 +0200

    temp fix throw

--- Assets/Scripts/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Player/PlayerAnimationController.cs
@@ -5,16 +5,28 @@ public class PlayerAnimationController : MonoBehaviour
     public Animator m_animator;
     public PlayerControl m_control;
     public Rigidbody m_rb;
+    public TreeSensor m_treeSensor;
+    public Damageable m_damageable;
 
 
-    void Start()
+    private void Start()
     {
-        
+        m_treeSensor.m_chopEvent.AddListener(Chop);
     }
 
-    
     void Update()
     {
         m_animator.SetBool("IsWalking", m_rb.velocity.sqrMagnitude > 0);
+        m_animator.SetBool("IsSawing", m_treeSensor.IsChoppingTree());
+        m_animator.SetBool("IsStun", m_damageable.IsStunned());
+    }
+
+    void Chop()
+    {
+        if (!m_animator.GetBool("IsSawing"))
+        {
+            m_animator.SetTrigger("TSawing");
+            m_animator.SetBool("IsSawing", true);
+        }
     }
 }
