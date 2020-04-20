f245e1b4aed88b297f23f8bec581b3a0825f2205
LucasPejoux <lucas.pejoux.03@hotmail.fr>
2020-04-21 01:42:33 +0200

    poute

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -1,25 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class UIMainMenu : MonoBehaviour
 {
 
     public GameObject canvasTuto;
     public ChangeSceneFromUI goGame;
+    public EventSystem m_eventSys;
+    public GameObject firstSelMainMenu;
+    bool canPass =false;
 
     void Start()
     {
         canvasTuto.SetActive(false);
-
+        m_eventSys.SetSelectedGameObject(firstSelMainMenu);
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton1)) && canvasTuto.activeSelf)
+        if ((Input.GetKeyDown(KeyCode.Mouse0) || Input.GetKeyDown(KeyCode.JoystickButton1)) && canvasTuto.activeSelf && canPass)
         {
-            canvasTuto.SetActive(false);
             goGame.LoadGame();
         }
     }
@@ -28,5 +33,6 @@ public class UIMainMenu : MonoBehaviour
     public void DisplayTuto()
     {
         canvasTuto.SetActive(true);
+        canPass = true;
     }
 }
