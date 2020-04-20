e0f3ab4c82621031d2c83fa4f3b069d2c80b8940
LucasPejoux <lucas.pejoux.03@hotmail.fr>
2020-04-20 16:42:43 +0200

    add script selected eventsys

---
+++ Assets/Scripts/UI/NathanScriptPourFirstSelectedMerci.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NathanScriptPourFirstSelectedMerci : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject lastselect;
+
+    void Start()
+    {
+        lastselect = null;
+    }
+
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            EventSystem.current.SetSelectedGameObject(lastselect);
+        }
+        else
+        {
+            lastselect = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
